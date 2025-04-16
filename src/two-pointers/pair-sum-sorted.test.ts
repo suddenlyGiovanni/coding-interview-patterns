0da6819b97d95ebc419e9d977a6cd2492729237c
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-04-16 18:35:52 +0200

    refactor(two-pointers): improve type definitions and enhance pair-sum handling logic

--- src/two-pointers/pair-sum-sorted.test.ts
+++ src/two-pointers/pair-sum-sorted.test.ts
@@ -1,7 +1,10 @@
 import { describe, expect, it } from 'vitest'
 
 
-type PairSumSortedStrategy = <Nums extends readonly number[], Idx extends keyof Nums>(nums: Nums, target: number) => [left: Idx, Idx][];
+type PointerTuple = readonly [leftPointer: number, rightPointer: number]
+
+
+type PairSumSortedStrategy = (nums: readonly number[], target: number) => PointerTuple[];
 
 /**
  * Brute force solution that involves checking all possible pairs;
@@ -13,10 +16,17 @@ type PairSumSortedStrategy = <Nums extends readonly number[], Idx extends keyof 
  */
 const bruteForce: PairSumSortedStrategy = (nums, target) => {
 	let result: [number, number][] = []
-	for (let left = 0; left < nums.length; left++) {
-		for (let right = left + 1; right < nums.length; right++) {
-			if (nums[left] + nums[right] === target) {
-				result.push([left, right])
+
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			const left = nums.at(i)
+			const right = nums.at(j)
+
+			if (left === undefined || right === undefined) break
+
+
+			if (left + right === target) {
+				result.push([i, j])
 			}
 		}
 	}
@@ -25,21 +35,25 @@ const bruteForce: PairSumSortedStrategy = (nums, target) => {
 
 /**
  * Given an array of integers sorted in ascending order and a target value;
- * We can find the indexes of any pair of numbers in the array that sum to the target value with the two pointers technique.
+ * We can find the indexes of any pair of numbers in the array that sum to the target value with the two-pointer technique.
  *
- * For any pair of valuse, of left and right:
- * - if the sum is less than the target, increment left, aiming to increas the sum toward the target value;
- * - if the sum is grater that the target, decrement right aiming to decreas the sum toward the target value;
+ * For any pair of values, of left and right:
+ * - if the sum is less than the target, increment left, aiming to increase the sum toward the target value;
+ * - if the sum is greater that the target, decrement right aiming to decrease the sum toward the target value;
  * - if the sum is equal to the target, we have found a pair, so we can store the indexes and increment left;
  */
 const twoPointers: PairSumSortedStrategy = (nums, target) => {
 	let leftPointer = 0
 	let rightPointer = nums.length - 1
-	const result: [number, number][] = []
+	const result: PointerTuple[] = []
 
 	while (leftPointer < rightPointer) {
 		const left = nums[leftPointer]
 		const right = nums[rightPointer]
+		if (left === undefined || right === undefined) {
+			break
+		}
+
 		const sum = left + right
 		if (sum < target) {
 			leftPointer++
@@ -55,27 +69,31 @@ const twoPointers: PairSumSortedStrategy = (nums, target) => {
 	return result
 }
 
+
 const recursiveTwoPointers: PairSumSortedStrategy = (nums, target) => {
-	// base case: left pointer is greater than or equal to right pointer; no pair found; stop recursion
+	// base case: the left pointer is greater than or equal to the right pointer; no pair found; stop recursion
 
-	function findPairsWithTargetSum<Idx extends keyof typeof nums, LeftPointer extends Idx, RightPointer extends Idx>(leftPoiner: LeftPointer, rightPointer: RightPointer, pairs: [LeftPointer, RightPointer][]): [LeftPointer, RightPointer][] {
-		if (leftPoiner >= rightPointer) return pairs
+	function findPairsWithTargetSum(leftPointer: number, rightPointer: number, pairs: (readonly [leftPointer: number, rightPointer: number])[]) {
+		if (leftPointer >= rightPointer) return pairs
 
-		const left = nums[leftPoiner]
-		const right = nums[rightPointer]
+		const left = nums[leftPointer]!
+		const right = nums[rightPointer]!
+
 		const sum = left + right
 
 		if (sum < target) {
-			return findPairsWithTargetSum(leftPoiner + 1, rightPointer, pairs)
+			return findPairsWithTargetSum(leftPointer + 1, rightPointer, pairs)
 		}
 
 		if (sum > target) {
-			return findPairsWithTargetSum(leftPoiner, rightPointer - 1, pairs)
+			return findPairsWithTargetSum(leftPointer, rightPointer - 1, pairs)
 		}
 
 		if (sum === target) {
-			return findPairsWithTargetSum(leftPoiner + 1, rightPointer, [...pairs, [leftPoiner, rightPointer]])
+			return findPairsWithTargetSum(leftPointer + 1, rightPointer, [...pairs, [leftPointer, rightPointer]])
 		}
+
+		return pairs
 
 	}
 
@@ -103,9 +121,9 @@ describe('pairSumSorted', () => {
 
 	const strategies = [bruteForce, twoPointers, recursiveTwoPointers] as const
 	describe('test an empty array', () => {
-		const nums = []
+		const nums: number[] = []
 		const target = 0
-		const expectedOutput = []
+		const expectedOutput: PointerTuple[] = []
 
 		it.for(strategies)('with %O strategy', (strategy) => {
 			const pairSumSorted = makePairSumSorted(strategy)
@@ -116,7 +134,7 @@ describe('pairSumSorted', () => {
 	describe('test an array with just one element', () => {
 		const nums = [1]
 		const target = 1
-		const expectedOutput = []
+		const expectedOutput: PointerTuple[] = []
 
 		it.for(strategies)('with %O strategy', (strategy) => {
 			const pairSumSorted = makePairSumSorted(strategy)
@@ -127,7 +145,7 @@ describe('pairSumSorted', () => {
 	describe('test two-element array that contains a pair that sums to the target', () => {
 		const nums = [2, 4]
 		const target = 5
-		const expectedOutput = []
+		const expectedOutput: PointerTuple[] = []
 
 		it.for(strategies)('with %O strategy', (strategy) => {
 			const pairSumSorted = makePairSumSorted(strategy)
@@ -139,37 +157,37 @@ describe('pairSumSorted', () => {
 	describe('test an array with duplicate values', () => {
 		const nums = [2, 2, 3]
 		const target = 5
-		const expectedOutputs = [[0, 2], [1, 2]] as const
+		const expectedOutputs: PointerTuple[] = [[0, 2], [1, 2]]
 
 		it.for(strategies)('with %O strategy', (strategy) => {
 			const pairSumSorted = makePairSumSorted(strategy)
 			const results = pairSumSorted(nums, target)
-			expect(results).toContainEqual(expectedOutputs[0])
-			expect(results).toContainEqual(expectedOutputs[1])
+			expect(results).toEqual(expect.arrayContaining(expectedOutputs))
+
 		})
 	})
 
 	describe('test if the algorithm works with negative number in the target pair', () => {
 		const nums = [-1, 2, 3]
 		const target = 2
-		const expectedOutput = [0, 2]
+		const expectedOutput: PointerTuple[] = [[0, 2]]
 
 		it.for(strategies)('with %O strategy', (strategy) => {
 			const pairSumSorted = makePairSumSorted(strategy)
 			const results = pairSumSorted(nums, target)
-			expect(results).toContainEqual(expectedOutput)
+			expect(results).toEqual(expect.arrayContaining(expectedOutput))
 		})
 	})
 
 	describe('test if the algorithm works with both numbers of the target pair being negative', () => {
 		const nums = [-3, -2, -1]
 		const target = -5
-		const expectedOutput = [0, 1]
+		const expectedOutput: PointerTuple[] = [[0, 1]]
 
 		it.for(strategies)('with %O strategy', (strategy) => {
 			const pairSumSorted = makePairSumSorted(strategy)
 			const results = pairSumSorted(nums, target)
-			expect(results).toContainEqual(expectedOutput)
+			expect(results).toEqual(expect.arrayContaining(expectedOutput))
 		})
 	})
 
